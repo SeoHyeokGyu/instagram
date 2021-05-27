efe526ba4b26acb062eec275daf8d7bc0e7b3dc2
SeoHyeokGyu <shk64099@gmail.com>
2021-05-28 00:44:25 +0900

    #Finish Login Page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,26 +1,31 @@
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import FirebaseContext from "../context/firebase";
 import { useContext, useEffect, useState } from "react";
 import React from "react";
+import * as ROUTES from "../constrants/routes";
 
 export default function Login() {
-  // eslint-disable-next-line no-unused-vars
   const history = useHistory();
-  // eslint-disable-next-line no-unused-vars
   const { firebase } = useContext(FirebaseContext);
 
-  // eslint-disable-next-line no-unused-vars
   const [emailAddress, setEmailAddress] = useState("");
-  // eslint-disable-next-line no-unused-vars
   const [password, setPassword] = useState("");
 
-  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState("");
-  // eslint-disable-next-line no-unused-vars
   const isInvalid = password === "" || emailAddress === "";
 
-  // eslint-disable-next-line no-unused-vars
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = "Login -Instagram";
@@ -36,7 +41,50 @@ export default function Login() {
         ></img>
       </div>
       <div className="flex flex-col w-2/5">
-        <p>I will be the form</p>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              area-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <input
+              area-label="Enter your password"
+              type="text"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+            ${isInvalid && "opacity-50"}`}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            Don't have an account?{` `}
+            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
