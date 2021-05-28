f96de81fc7616c204df81f62aa9df5cae6aa0f33
SeoHyeokGyu <shk64099@gmail.com>
2021-05-28 18:02:40 +0900

    # Created Timeline Component, Sidebar Component, Header Component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import * as ROUTES from './constrants/routes';
 const Login = lazy(() => import('./pages/login'));
 const SignUp = lazy(() => import('./pages/sign-up'));
 const NotFound = lazy(() => import('./pages/not-found'));
+const Dashboard = lazy(() => import('./pages/dashboard'));
 
-function App() {
+export default function App() {
   return (
     <Router>
       <Suspense fallback={<p>Loading...</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
           <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} />
           <Route component={NotFound} />
         </Switch>
       </Suspense>
     </Router>
   );
 }
-
-export default App;
