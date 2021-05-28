c981629d6c52be8b2801e5373680df2be8a6cb91
SeoHyeokGyu <shk64099@gmail.com>
2021-05-28 16:45:13 +0900

    #Finish Signup Page

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,14 +1,14 @@
-import Firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDy2cyCRA97quBqxRJhDUkUiVWxndar0xQ",
-  authDomain: "instagram-clone-2738c.firebaseapp.com",
-  projectId: "instagram-clone-2738c",
-  storageBucket: "instagram-clone-2738c.appspot.com",
-  messagingSenderId: "9779505480",
-  appId: "1:9779505480:web:e64f49d40941ff82196b7d",
+  apiKey: 'AIzaSyDy2cyCRA97quBqxRJhDUkUiVWxndar0xQ',
+  authDomain: 'instagram-clone-2738c.firebaseapp.com',
+  projectId: 'instagram-clone-2738c',
+  storageBucket: 'instagram-clone-2738c.appspot.com',
+  messagingSenderId: '9779505480',
+  appId: '1:9779505480:web:e64f49d40941ff82196b7d'
 };
 
 const firebase = Firebase.initializeApp(config);
--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,31 +4,27 @@ import React, { useContext, useEffect, useState } from 'react';
 import FirebaseContext from '../context/firebase';
 import * as ROUTES from '../constrants/routes';
 
-export default function Login() {
+export default function SignUp() {
   const history = useHistory();
   const { firebase } = useContext(FirebaseContext);
 
+  const [username, setUsername] = useState('');
+  const [fullname, setFullname] = useState('');
   const [emailAddress, setEmailAddress] = useState('');
   const [password, setPassword] = useState('');
 
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = async (event) => {
+  const handleSignup = async (event) => {
     event.preventDefault();
 
-    try {
-      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
-      history.push(ROUTES.DASHBOARD);
-    } catch (error) {
-      setEmailAddress('');
-      setPassword('');
-      setError(error.message);
-    }
+    // try {
+    // } catch (error) {}
   };
 
   useEffect(() => {
-    document.title = 'Login -Instagram';
+    document.title = 'Sign Up -Instagram';
   }, []);
 
   return (
@@ -47,13 +43,30 @@ export default function Login() {
           </h1>
           {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-          <form onSubmit={handleLogin} method="POST">
+          <form onSubmit={handleSignup} method="POST">
+            <input
+              area-label="Enter your fullname"
+              type="text"
+              placeholder="fullname"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setFullname(target.value)}
+              value={fullname || ''}
+            />
+            <input
+              area-label="Enter your username"
+              type="text"
+              placeholder="username"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
+              onChange={({ target }) => setUsername(target.value)}
+              value={username || ''}
+            />
             <input
               area-label="Enter your email address"
               type="text"
               placeholder="Email address"
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
               onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress || ''}
             />
             <input
               area-label="Enter your password"
@@ -61,6 +74,7 @@ export default function Login() {
               placeholder="Password"
               className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border-gray-primary rounded mb-2"
               onChange={({ target }) => setPassword(target.value)}
+              value={password || ''}
             />
             <button
               disabled={isInvalid}
@@ -68,15 +82,16 @@ export default function Login() {
               className={`bg-blue-medium text-white w-full rounded h-8 font-bold
             ${isInvalid && 'opacity-50'}`}
             >
-              Login
+              SIgn Up
             </button>
           </form>
         </div>
         <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
           <p className="text-sm">
-            Don't have an account?{` `}
-            <Link to={ROUTES.SIGN_UP} className="font-bold text-blue-medium">
-              Sign up
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            Have an account?{` `}
+            <Link to={ROUTES.LOGIN} className="font-bold text-blue-medium">
+              Login
             </Link>
           </p>
         </div>
